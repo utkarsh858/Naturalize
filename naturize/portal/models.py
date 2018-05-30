8ad2b938d68dac1a95cb7ac806b962c500432676
utkarsh858 <utkarsh858iitr@gmail.com>
2018-05-30 15:31:29 +0530

    Added the admin site

--- naturize/portal/models.py
+++ naturize/portal/models.py
@@ -3,9 +3,5 @@ from django.db import models
 # Create your models here.
 class MainModel(models.Model):
 	"""docstring for MainModel"""
-	def __init__(self, arg):
-		super(MainModel, self).__init__()
-		self.arg = arg
-		
 	aadhar_number = models.CharField(max_length=200)
 	password = models.CharField(max_length=200)
