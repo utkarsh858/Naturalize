f8a2a883b4d6941bdb142b3a5377cfe9d8a78276
utkarsh858 <utkarsh858iitr@gmail.com>
2018-05-30 14:50:22 +0530

    Created Models, migrations and added the app to the website

--- naturize/portal/models.py
+++ naturize/portal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class MainModel(models.Model):
+	"""docstring for MainModel"""
+	def __init__(self, arg):
+		super(MainModel, self).__init__()
+		self.arg = arg
+		
+	aadhar_number = models.CharField(max_length=200)
+	password = models.CharField(max_length=200)
