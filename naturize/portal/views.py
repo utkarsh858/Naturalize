51caf307757de5b78acf54a3852ff638c1a1f7a7
utkarsh858 <utkarsh858iitr@gmail.com>
2018-05-31 09:23:30 +0530

    Created a basic django login portal

--- naturize/portal/views.py
+++ naturize/portal/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
+import hashlib
+from .models import MainModel
+# import django.template.loader
 # Create your views here.
+
+def hash_password(password):
+    # uuid is used to generate a random number
+    salt = uuid.uuid4().hex
+    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+
+def check_password(hashed_password, user_password):
+    password, salt = hashed_password.split(':')
+    return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+
+
 def index(request):
-	return HttpResponse("Login Now")
+	template = loader.get_template('portal/index.html')
+	return HttpResponse(template.render({},request) )
+
+def dashboard(request):
+	template = loader.get_template('portal/index.html')
+	if (request.post['aadhar']=='') or (request.post['password']==''):
+		return HttpResponse(template.render({'error':"Partially filled form"},request))
+
+	try:
+		selected_row = MainModel.objects.get(aadhar_number = request.post['aadhar'])
+	except (KeyError, MainModel.DoesNotExist):
+		return HttpResponse(template.render({'error':"Incorrect Aadhar or Password"},request))
+	else:
+		if check_password(selected_row.password , request.post['password']):
+			teplate2 = loader.get_template('portal/dashboard.html')
+			return HttpResponse(teplate2.render({'points':""},request))
+		else:
+			return HttpResponse(template.render({'error':"Incorrect Aadhar or Password"},request))
